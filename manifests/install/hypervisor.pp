fdf8e1430ca575ceacae1dc0a8e9b061816ee15f
Taylor Nekroze Lawson <tlawson@nekroze.com>
2014-11-29 17:16:06 +1000

    Added ganeti::install::hypervisor

---
+++ manifests/install/hypervisor.pp
@@ -0,0 +1,12 @@
+# == Class ganeti::install::hypervisor
+#
+# This class is called from ganeti for installing the hypervisor.
+#
+class ganeti::install::hypervisor {
+  
+  $packages = $ganeti::packages_kvm
+  
+  package { $packages:
+    ensure => present,
+  }
+}
