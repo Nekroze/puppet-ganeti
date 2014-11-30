9c42d93298ca3c13ad6ffe87f04897b261cff811
Taylor Nekroze Lawson <tlawson@nekroze.com>
2014-11-30 12:21:57 +1000

    added full param validation and changed all package params to lists

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,8 +9,8 @@
 #
 class ganeti (
   $ensure          = $ganeti::params::ensure,
-  $package_name    = $ganeti::params::package_name,
   $service_name    = $ganeti::params::service_name,
+  $packages_ganeti = $ganeti::params::packages_ganeti,
   $packages_drbd   = $ganeti::params::packages_drbd,
   $packages_kvm    = $ganeti::params::packages_kvm,
   $web_ensure      = $ganeti::params::web_ensure,
@@ -19,6 +19,8 @@ class ganeti (
 ) inherits ganeti::params {
 
   validate_string($ensure, $web_ensure, $web_version)
+  validate_array($packages_ganeti, $packages_drbd, $packages_kvm)
+  validate_absolution_path($web_install_dir)
 
   class { 'ganeti::install': } ->
   class { 'ganeti::config': } ~>
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,16 +4,16 @@
 # It sets variables according to platform.
 #
 class ganeti::params {
-  $ensure          = 'present',
+  $ensure            = 'present',
   if $::osfamily == 'Debian' {
-    $package_name  = 'ganeti2'
+    $packages_ganeti = 'ganeti2'
   } else {
-    $package_name  = 'ganeti'
+    $packages_ganeti = 'ganeti'
   }
-  $service_name    = 'ganeti'
-  $packages_drbd   = ['drbd84-utils', 'kmod-drbd84']
-  $packages_kvm    = ['qemu-kvm', 'libvirt', 'python-virtinst', 'bridge-utils']
-  $web_version     = '0.10.2',
-  $web_ensure      = 'present',
-  $web_install_dir = '/usr/src',
+  $service_name      = 'ganeti'
+  $packages_drbd     = ['drbd84-utils', 'kmod-drbd84']
+  $packages_kvm      = ['qemu-kvm', 'libvirt', 'python-virtinst', 'bridge-utils']
+  $web_version       = '0.10.2',
+  $web_ensure        = 'present',
+  $web_install_dir   = '/usr/src',
 }
