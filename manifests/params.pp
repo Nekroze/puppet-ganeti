6ff4f67f8d67f52ce2088b5cf56495f7f7c9822a
Taylor Nekroze Lawson <tlawson@nekroze.com>
2014-11-30 16:04:07 +1000

    added template for ldap web manager settings and required params with validation

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,35 +4,42 @@
 # It sets variables according to platform.
 #
 class ganeti::params {
-  $ensure            = 'present',
+  $ensure             = 'present',
   if $::osfamily == 'Debian' {
-    $packages_ganeti = 'ganeti2'
+    $packages_ganeti     = 'ganeti2'
   } else {
-    $packages_ganeti = 'ganeti'
+    $packages_ganeti     = 'ganeti'
   }
-  $service_name      = 'ganeti'
-  $packages_drbd     = ['drbd84-utils', 'kmod-drbd84']
-  $drbd_ensure       = 'present',
-  $packages_kvm      = ['qemu-kvm', 'libvirt', 'python-virtinst', 'bridge-utils']
-  $kvm_ensure        = 'present',
-  $web_version       = '0.10.2',
-  $web_ensure        = 'present',
-  $web_install_dir   = '/usr/src',
-  $web_admins        = [],
-  $web_database_type = 'postgresql',
-  $web_database_name = 'ganeti-web',
-  $web_database_user = 'ganeti-web',
-  $web_database_pass = 'ganeti-web',
-  $web_database_host = 'database',
-  $web_database_port = 5432,
-  $web_timezone      = 'UTC',
-  $web_language      = 'en_US',
-  $web_id            = 1,
-  $web_name          = 'Ganeti Web Management',
-  $web_email_host    = 'mx',
-  $web_email_port    = 25,
-  $web_email_from    = "noreply@$fqdn",
-  $web_registration  = true,
-  $web_api_key       = 'CHANGE_ME',
-  $web_ldap          = true,
+  $service_name          = 'ganeti'
+  $packages_drbd         = ['drbd84-utils', 'kmod-drbd84']
+  $drbd_ensure           = 'present',
+  $packages_kvm          = ['qemu-kvm', 'libvirt', 'python-virtinst', 'bridge-utils']
+  $kvm_ensure            = 'present',
+  $web_version           = '0.10.2',
+  $web_ensure            = 'present',
+  $web_install_dir       = '/usr/src',
+  $web_admins            = [],
+  $web_database_type     = 'postgresql',
+  $web_database_name     = 'ganeti-web',
+  $web_database_user     = 'ganeti-web',
+  $web_database_pass     = 'ganeti-web',
+  $web_database_host     = 'database',
+  $web_database_port     = 5432,
+  $web_timezone          = 'UTC',
+  $web_language          = 'en_US',
+  $web_id                = 1,
+  $web_name              = 'Ganeti Web Management',
+  $web_email_host        = 'mx',
+  $web_email_port        = 25,
+  $web_email_from        = "noreply@$fqdn",
+  $web_registration      = true,
+  $web_api_key           = 'CHANGE_ME',
+  $web_ldap              = true,
+  $web_ldap_uri          = 'ldaps://ldap',
+  $web_ldap_bind_dn      = 'read',
+  $web_ldap_bind_pass    = 'read',
+  $web_ldap_users_dn     = '',
+  $web_ldap_users_uid    = 'uid',
+  $web_ldap_groups_dn    = '',
+  $web_ldap_groups_class = 'groupOfNames',
 }
