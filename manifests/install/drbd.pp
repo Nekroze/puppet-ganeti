40d132d1b6ebd2bde0298d0fa7373aa22be3026f
Taylor Nekroze Lawson <tlawson@nekroze.com>
2014-11-30 12:11:25 +1000

    corrected documentation

--- manifests/install/drbd.pp
+++ manifests/install/drbd.pp
@@ -1,6 +1,6 @@
-# == Class ganeti::install::hypervisor
+# == Class ganeti::install:drbd
 #
-# This class is called from ganeti for installing the hypervisor.
+# This class is called from ganeti for installing the drbd requirements.
 #
 class ganeti::install::drbd {
   
