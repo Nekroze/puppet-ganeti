e0db493b9d6012a0b324135f1a12905bf8475510
Taylor Nekroze Lawson <tlawson@nekroze.com>
2014-11-30 15:35:17 +1000

    added handling of disable ldap

--- manifests/install/web.pp
+++ manifests/install/web.pp
@@ -3,14 +3,6 @@
 # This class is called from ganeti for installing the web manager.
 #
 class ganeti::install::web {
-  if $ganeti::web_ldap {
-    if $::osfamily == 'Redhat' {
-      $ldap_packages = ['openldap-devel']
-      } else {
-      $ldap_packages = ['libldap2-dev', 'libsasl2-dev']
-    }    
-    ensure_packages($ldap_packages, {'ensure' => $ganeti::web_ensure})
-  }
 
   python::pip { 'fabric':
     ensure   => $ganeti::web_ensure,
@@ -23,10 +15,34 @@ class ganeti::install::web {
     target   => $ganeti::web_install_dir,
   }
   ->
-  exec { "install-ganeti-web":
+  exec { 'install-ganeti-web':
     cwd     => "${ganeti::web_install_dir}/ganeti-webmgr-${ganet::web_version}",
     command => "fab deploy \
-                && echo \"${ganeti::web_version}\" > \"/var/log/puppet/ganeti-web-installed-version\"",
+                && echo \"${ganeti::web_version}\" > /var/log/puppet/ganeti-web-installed-version",
     unless  => "test \"`cat  /var/log/puppet/ganeti-web-installed-version 2>/dev/null`\" = \"${ganeti::web_version}\"",
+  }
+
+  if $ganeti::web_ldap {
+    if $::osfamily == 'Redhat' {
+      $ldap_packages = ['openldap-devel']
+      } else {
+      $ldap_packages = ['libldap2-dev', 'libsasl2-dev']
+    }    
+    ensure_packages($ldap_packages, {'ensure' => $ganeti::web_ensure})
+
+    exec { "install-ganeti-ldap-enable":
+      cwd       => "${ganeti::web_install_dir}/ganeti-webmgr-${ganet::web_version}",
+      command   => "fab ldap \
+                    && echo \"${ganeti::web_version}+ldap\" > /var/log/puppet/ganeti-web-installed-version-ldap",
+      unless    => "test \"`cat  /var/log/puppet/ganeti-web-installed-version-ldap 2>/dev/null`\" = \"${ganeti::web_version}+ldap\"",
+      subscribe => Exec['install-ganeti-web'],
+    }
+  } else {
+    exec { "install-ganeti-ldap-disable":
+      cwd       => "${ganeti::web_install_dir}/ganeti-webmgr-${ganet::web_version}",
+      command   => 'fab ldap:disable \
+                    && rm -f /var/log/puppet/ganeti-web-installed-version-ldap',
+      onlyif    => 'test -f /var/log/puppet/ganeti-web-installed-version-ldap',
+    }
   }
 }
