b9c76d4ce00291336ebbd44dc3bd7544878bb489
Taylor Nekroze Lawson <tlawson@nekroze.com>
2014-11-30 12:33:06 +1000

    Added python management for installation of fabric, required by ganeti web manager

--- manifests/install/web.pp
+++ manifests/install/web.pp
@@ -4,6 +4,10 @@
 #
 class ganeti::install::web {
 
+  python::pip { 'fabric':
+    ensure   => $ganeti::web_ensure,
+  }
+  ->
   archive { 'ganeti-${ganeti::web_version}':
     ensure   => $ganeti::web_ensure,
     url      => "https://code.osuosl.org/attachments/download/3321/ganeti-${ganeti::web_version}.tar.gz",
